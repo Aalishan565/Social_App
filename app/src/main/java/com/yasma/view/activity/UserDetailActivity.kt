2ce6cbe9440c71b8c20186ee5861cc35c313991b
Aalishan <aalishan565@gmail.com>
2019-11-21 22:05:39 +0530

    -user detail data population

--- app/src/main/java/com/yasma/view/activity/UserDetailActivity.kt
+++ app/src/main/java/com/yasma/view/activity/UserDetailActivity.kt
@@ -1,20 +1,22 @@
 package com.yasma.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.yasma.R
 import com.yasma.appUtils.MyAppConstant
 import com.yasma.dto.User
 import com.yasma.listeners.UserDetailActivityViewListener
 import com.yasma.presenterImplModels.UserDetailActivityPresenterImpl
-import com.yasma.presenters.UserDetailActivityPresenter
+import kotlinx.android.synthetic.main.activity_user_detail.*
+import kotlinx.android.synthetic.main.row_item_post_detail.*
 
 class UserDetailActivity : AppCompatActivity(), UserDetailActivityViewListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_detail)
+        toolbar.title = getString(R.string.user_detail_activity_title)
         val bundle: Bundle? = intent.extras
         val userId = bundle?.get(MyAppConstant.USER_ID)
         val userDetailActivityPresenter = UserDetailActivityPresenterImpl(this)
@@ -22,7 +24,9 @@ class UserDetailActivity : AppCompatActivity(), UserDetailActivityViewListener {
     }
 
     override fun successResponse(body: User?) {
-        Toast.makeText(this, "" + body.toString(), Toast.LENGTH_SHORT).show()
+        tvName.text = body?.name
+        tvEmail.text = body?.email
+        tvBody.text = body?.address.toString()
     }
 
     override fun failureResponse(message: String) {
